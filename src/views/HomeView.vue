40ed83c3286ceb8b8c42fdab36cd26a0a612d192
xaoyaoo <xaoyaoo@qq.com>
2024-01-02 17:40:58 +0800

    添加路由

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,7 @@
 </script>
 
 <template>
-  <div class="init-right-area"
-       style="background-color: #d2d2fa; height: 100vh; display: grid; place-items: center; ">
+  <div style="background-color: #d2d2fa; height: 100vh; display: grid; place-items: center; ">
     <h2 style="text-align: center">欢迎使用<a href="https://github.com/xaoyaoo/PyWxDump.git">PyWxDump</a>聊天记录查看工具!
     </h2>
   </div>
