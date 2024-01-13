919e05b645b1af9c2aba1b8e8ff7d252f08b05b3
xaoyaoo <xaoyaoo@qq.com>
2024-01-13 20:19:12 +0800

    将关于帮助设置，放到other目录

--- src/router/index.ts
+++ src/router/index.ts
@@ -54,7 +54,7 @@ const router = createRouter({
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import('@/views/other/AboutView.vue')
+            component: () => import((`@/views/other/AboutView.vue`))
         },
         {
             path: '/help',
