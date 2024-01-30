8857691130864bc307e71a1dcf2323664d619484
xaoyaoo <xaoyaoo@qq.com>
2024-01-30 11:47:29 +0800

    增加开发自动转发api端口到5000

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,18 @@ export default defineConfig({
         }
     },
     base: './',
+    server: {
+        open: false,
+        port: 5171,
+        https: undefined,
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:5000",
+                changeOrigin: true, //是否跨域
+                // rewrite: (path) => path.replace(/^\/mis/, ""), //因为后端接口有mis前缀，所以不需要替换
+                // ws: true,                       //是否代理 websockets
+                // secure: true, //是否https接口
+            },
+        },
+    },
 })
