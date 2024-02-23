94b13173325331adf83c5eb090981be99b6c7e7f
xaoyaoo <xaoyaoo@qq.com>
2024-02-24 00:51:55 +0800

    html显示聊天记录

--- src/Appexport.vue
+++ src/Appexport.vue
@@ -50,6 +50,7 @@ const userData = ref<User>({
 // 定义变量
 const messages = ref<msg[]>([]);
 const userlist = ref<UserList>({});
+const msg_list = ref<msg[]>([]);
 const my_wxid = ref('');
 const limit = ref(50);
 const start = ref(0);
@@ -58,25 +59,16 @@ const start = ref(0);
 // 在组件挂载后加载 JSON 文件
 onMounted(async () => {
   try {
-    const response1 = await fetch('./data/userdata.json');
+    const response1 = await fetch('./data/msg_user.json');
     if (!response1.ok) {
       throw new Error('Failed to load JSON file');
     }
-
-    console.log(response1);
-    console.log("response1")
     const data1 = await response1.json();
-    console.log(data1);
-    userData.value = data1.myUserData;
-    userlist.value = data1.userlist;
-
-    const response2 = await fetch('./data/data.json');
-    console.log(response2);
-    if (!response2.ok) {
-      throw new Error('Failed to load JSON file');
-    }
-     messages.value =  await response2.json();
-
+    userData.value = data1.myuserdata;
+    my_wxid.value = data1.my_wxid;
+    userlist.value = data1.user_list;
+    msg_list.value = data1.msg_list;
+    messages.value = msg_list.value.slice(0,limit.value);
   } catch (error) {
     console.error('Error loading JSON file:', error);
   }
@@ -84,11 +76,11 @@ onMounted(async () => {
 // END 获取聊天记录
 
 // 这部分为构造消息的发送时间和头像
-const _direction = (msg:any) => {
+const _direction = (msg: any) => {
   if (msg.talker == '我') {
     msg.talker = my_wxid.value;
   }
-  const sendname = (msg:any) => {
+  const sendname = (msg: any) => {
     if (!userlist.value.hasOwnProperty(msg.talker)) {
       return msg.talker;
     }
@@ -121,14 +113,10 @@ const get_head_url = (msg: any) => {
 //  循环请求获取全部数据
 const loadMore = async () => {
   let limit1 = limit.value;
-  let start1 = start.value - limit1;
-  const body_data = await http.post('/api/msgs', {
-    start: start1,
-    limit: limit1,
-    wxid: userData.value.username,
-  });
+  let start1 = start.value + limit1;
   start.value = start1;
-  messages.value = body_data.msg_list.concat(messages.value);
+
+  messages.value = msg_list.value.slice(start1, start1 + limit1);
   // 排序
   messages.value.sort((a, b) => {
     return a.id - b.id;
@@ -137,53 +125,44 @@ const loadMore = async () => {
   messages.value = messages.value.filter((item, index, array) => {
     return index === 0 || item.id !== array[index - 1].id;
   });
-  userlist.value = Object.assign(userlist.value, body_data.user_list);
-
-  // await nextTick(() => {
-  //   updateScrollTop()
-  // })
 };
-
-// defineExpose({
-//   loadMore
-// })
 
 </script>
 
 <template>
   <div id="chat">
     <div class="chat_body">
-      <div class="chat_window" ref="chatWindow">
-        <!--    加载更多    -->
-        <div class="load_more" v-if="messages.length<userData.chat_count"
-             style="display: flex; justify-content: center; margin-top: 10px;margin-bottom: 10px;">
-          <el-button type="primary" @click="loadMore">查看更多消息</el-button>
-        </div>
 
+      <div class="chat_window" ref="chatWindow">
         <div class="message" v-for="(msg,index) in messages" :key="index">
           <!-- 文字消息 -->
           <MessageText v-if="msg.type_name == '文本'" :is_sender="msg.is_sender" :direction="_direction(msg)"
                        :headUrl="get_head_url(msg)" :content="msg.content.msg"></MessageText>
           <!-- 图片消息 -->
           <MessageImg v-else-if="msg.type_name == '图片'" :is_sender="msg.is_sender" :direction="_direction(msg)"
-                      :headUrl="get_head_url(msg)" :src="'/api/img?img_path='+msg.content.src"></MessageImg>
+                      :headUrl="get_head_url(msg)" :src="msg.content.src"></MessageImg>
           <!-- 表情消息 -->
           <MessageEmoji v-else-if="msg.type_name == '动画表情'" :is_sender="msg.is_sender" :direction="_direction(msg)"
                         :headUrl="get_head_url(msg)" :src="msg.content.src"></MessageEmoji>
           <!-- 视频消息 -->
           <MessageVideo v-else-if="msg.type_name == '视频'" :is_sender="msg.is_sender" :direction="_direction(msg)"
                         :headUrl="get_head_url(msg)" :src="msg.content.src"></MessageVideo>
-          <!-- 文件消息 -->
-          <MessageFile v-else-if="msg.type_name == '文件'" :is_sender="msg.is_sender" :direction="_direction(msg)"
-                       :headUrl="get_head_url(msg)" :src="msg.content.src"></MessageFile>
+<!--          &lt;!&ndash; 文件消息 &ndash;&gt;-->
+<!--          <MessageFile v-else-if="msg.type_name == '文件'" :is_sender="msg.is_sender" :direction="_direction(msg)"-->
+<!--                       :headUrl="get_head_url(msg)" :src="msg.content.src"></MessageFile>-->
           <!-- 语音消息 -->
           <MessageAudio v-else-if="msg.type_name == '语音'" :is_sender="msg.is_sender" :direction="_direction(msg)"
-                        :headUrl="get_head_url(msg)" :src="'/api/'+msg.content.src"
+                        :headUrl="get_head_url(msg)" :src="msg.content.src"
                         :msg="msg.content.msg"></MessageAudio>
           <!-- 其他消息 -->
           <MessageOther v-else :is_sender="msg.is_sender" :direction="_direction(msg)" :headUrl="get_head_url(msg)"
                         :content="msg.content.msg"></MessageOther>
 
+        </div>
+        <!--    加载更多    -->
+        <div class="load_more" v-if="messages.length<userData.chat_count"
+             style="display: flex; justify-content: center; margin-top: 10px;margin-bottom: 10px;">
+          <el-button type="primary" @click="loadMore">查看更多消息</el-button>
         </div>
 
       </div>
@@ -198,18 +177,12 @@ const loadMore = async () => {
   width: 100%;
   height: 100%;
   background-color: #f5f5f5;
-  display: flex;
-  flex-direction: column;
 
   .chat_body {
-    flex: 1;
-    overflow-y: hidden;
-    overflow-x: hidden;
 
     .chat_window {
       height: 100%;
-      overflow-y: scroll;
-      width: calc(100% + 17px);
+      width: calc(100%);
       padding: 0px;
       margin: 0px;
 
